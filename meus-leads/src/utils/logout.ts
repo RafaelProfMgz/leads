e791f2df6f45a2d7556d3613194129f5347ec6d3
RafaelProfMgz <154349177+RafaelProfMgz@users.noreply.github.com>
2025-03-10 15:10:17 -0400

    alterado o util de logout para apagar o user

--- meus-leads/src/utils/logout.ts
+++ meus-leads/src/utils/logout.ts
@@ -5,7 +5,7 @@ export const useLogout = () => {
 
   const logout = () => {
     localStorage.removeItem("token");
-    localStorage.removeItem("userName");
+    localStorage.removeItem("user");
 
     navigate("/"); 
   };
